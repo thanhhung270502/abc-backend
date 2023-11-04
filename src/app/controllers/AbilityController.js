61ca261bb465926072bfe73ed2cac203cf3166c8
Thanh Hùng Lý <thanhhung270502@gmail.com>
2023-11-04 11:27:46 +0700

    create - get ability

---
+++ src/app/controllers/AbilityController.js
@@ -0,0 +1,56 @@
+const pool = require('../../config/db');
+
+class AbilityController {
+    // [GET]
+    async index(req, res) {
+        try {
+            const query = 'SELECT * FROM ability';
+            const response = await pool.query(query);
+            return res.status(200).json(response.rows);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json('Internal Server Error');
+        }
+    }
+    // [GET]
+    async show(req, res, next) {
+        try {
+            const id = parseInt(req.params.slug);
+            const response = await pool.query('SELECT * FROM ability WHERE id = $1', [id]);
+            if (response.rows.length > 0) {
+                return res.status(200).json({
+                    message: 'Found user successfully',
+                    code: 200,
+                    body: {
+                        user: response.rows[0],
+                    },
+                });
+            } else {
+                return res.status(404).json({
+                    message: 'User not found',
+                    code: 404,
+                });
+            }
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json('Internal Server Error');
+        }
+    }
+    // [POST]
+    async create(req, res) {
+        try {
+            const { name } = req.body;
+            const response = await pool.query('INSERT INTO ability (name) VALUES ($1)', [name]);
+
+            return res.status(200).json({
+                message: 'User created successfully',
+                code: '200',
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json('Internal Server Error');
+        }
+    }
+}
+
+module.exports = new AbilityController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const siteRouter = require('./site');
 const usersRouter = require('./users');
 const authRouter = require('./auth');
+const abilityRouter = require('./ability');
 
 function route(app) {
+    app.use('/ability', abilityRouter);
     app.use('/auth', authRouter);
     app.use('/users', usersRouter);
     app.use('/', siteRouter);
