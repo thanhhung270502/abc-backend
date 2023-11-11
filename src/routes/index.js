0a15e89abf2f8f2f8a33fa70b73795b244f132c7
Thanh Hùng Lý <thanhhung270502@gmail.com>
2023-11-11 08:51:45 +0700

    login-user

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const usersRouter = require('./users');
 const authRouter = require('./auth');
 const abilityRouter = require('./ability');
 const projectsRouter = require('./projects');
+const sessionsRouter = require('./sessions');
 
 function route(app) {
     app.use('/ability', abilityRouter);
     app.use('/auth', authRouter);
     app.use('/users', usersRouter);
     app.use('/project', projectsRouter);
+    app.use('/sessions', sessionsRouter);
     app.use('/', siteRouter);
 }
 
