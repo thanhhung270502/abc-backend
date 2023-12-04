dbde1fe055bef5a142eb486d69aabda27a47d3fd
datdtnhcse <dat.dtnhcse@gmail.com>
2023-12-04 22:16:56 +0700

    apply and reject application

--- src/app/controllers/ProjectUsersController.js
+++ src/app/controllers/ProjectUsersController.js
@@ -1,6 +1,6 @@
 const pool = require('../../config/db');
 
-class AbilityController {
+class ProjectUsersController {
     // [GET]
     async index(req, res) {
         try {
@@ -29,6 +29,22 @@ class AbilityController {
             return res.status(500).json('Internal Server Error');
         }
     }
+    async getByUserID(req, res, next) {
+        try {
+            const id = parseInt(req.params.project_id);
+            const response = await pool.query(
+                `select *
+                from project p 
+                join project_user pu on pu.project_id = p.id
+                where pu.user_id = $1`,
+                [id],
+            );
+            return res.status(200).json(response.rows);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json('Internal Server Error');
+        }
+    }
     // [POST]
     async create(req, res) {
         try {
@@ -38,6 +54,7 @@ class AbilityController {
                 return res.status(400).json('Missing project_id or user_id');
             }
 
+            // handle existed application
             const resProjectUser = await pool.query(
                 'select * from project_user where project_id = $1 and user_id = $2',
                 [project_id, user_id],
@@ -47,6 +64,7 @@ class AbilityController {
                 return res.status(400).json({ message: 'Request is existed!' });
             }
 
+            // insert new application
             const response = await pool.query(
                 'INSERT INTO project_user (project_id, user_id, is_checked) VALUES ($1, $2, $3)',
                 [project_id, user_id, null],
@@ -79,23 +97,7 @@ class AbilityController {
                 });
             }
 
-            // const resProject = await pool.query('SELECT * FROM project WHERE id = $1', [project_id]);
-
-            // if (resProject.rows.length === 0) {
-            //     return res.status(404).json({
-            //         message: 'Project not found',
-            //         code: 404,
-            //     });
-            // }
-
-            // const resUser = await pool.query('SELECT * FROM users WHERE id = $1', [user_id]);
-            // if (resUser.rows.length === 0) {
-            //     return res.status(404).json({
-            //         message: 'User not found',
-            //         code: 404,
-            //     });
-            // }
-
+            // handle not existed application
             const checkProjectUser = await pool.query(
                 'SELECT * FROM project_user WHERE project_id = $1 AND user_id = $2',
                 [project_id, user_id],
@@ -107,6 +109,7 @@ class AbilityController {
                     .json(`ProjectUsers with project_id = ${project_id} and user_id = ${user_id} is not available`);
             }
 
+            // update application status 
             const response = await pool.query(
                 'UPDATE project_user SET is_checked = $1 WHERE project_id = $2 and user_id = $3',
                 [is_checked, project_id, user_id],
@@ -148,4 +151,4 @@ class AbilityController {
     }
 }
 
-module.exports = new AbilityController();
+module.exports = new ProjectUsersController();
