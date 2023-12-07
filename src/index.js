128e35213cf701355582035871cf204736bab840
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-12-07 13:20:26 +0700

    fixbug testcase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const route = require('./routes');
 const session = require('express-session');
-const passport = require('passport');
-const passportStrategy = require('./passport');
 require('dotenv').config();
 
 const app = express();
@@ -21,8 +19,6 @@ app.use(
     }),
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Routes
 route(app);
